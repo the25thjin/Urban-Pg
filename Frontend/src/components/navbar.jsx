e57677c0a854ee29ab43d767d6d600d4fe242db4
the25thjin <mannr075@gmail.com>
2024-08-14 12:25:51 +0530

    updated navbar

--- Frontend/src/components/navbar.jsx
+++ Frontend/src/components/navbar.jsx
@@ -5,7 +5,7 @@ function Navbar(){
     <>
     <div className="bg-black font-Poppins text-white flex justify-between items-center h-[93px]  w-screen fixed ">
         <div> <img src={logo} className="w-[140px] h-[60px] ml-4 cursor-pointer" alt="logo"  /> </div>
-        <div><ul className="flex justify-evenly text-xl gap-7 mr-6">
+        <div><ul className="flex justify-evenly text-xl gap-7 mr-10">
             <li className="cursor-pointer">Home</li>
             <li className="cursor-pointer">About Us</li>
             <li className="cursor-pointer">Testimonials</li>
