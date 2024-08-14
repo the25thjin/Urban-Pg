20ab571bda49d3e1a9f388cf5f087055ce5b99d0
the25thjin <mannr075@gmail.com>
2024-08-15 01:41:52 +0530

    footer added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react"
 import Navbar from "./components/navbar"
-
+import Hero from "./components/hero"
+import Footer from "./components/footer"
 function App() {
   return (
     <>
       <Navbar/>
+      <Hero/>
+      <Footer/>
     </>
   )
 }
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -7,7 +7,8 @@ export default {
   theme: {
     extend: {
       fontFamily:{
-        Poppins: "Poppins"
+        Poppins: "Poppins",
+        Inter : "Inter"
       },
       
     },
