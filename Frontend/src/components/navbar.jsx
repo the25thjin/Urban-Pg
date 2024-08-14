fc793b7c89d711be279bbea352468b6a7eadb23b
the25thjin <mannr075@gmail.com>
2024-08-14 10:40:45 +0530

    navbar done

---
+++ Frontend/src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import logo from "../assets/logo.png"
+
+function Navbar(){
+    return(
+    <>
+    <div className="bg-black font-Poppins text-white flex justify-between items-center h-[93px]  w-screen fixed ">
+        <div> <img src={logo} className="w-[140px] h-[60px] ml-4 cursor-pointer" alt="logo"  /> </div>
+        <div><ul className="flex justify-evenly text-xl gap-7 mr-6">
+            <li className="cursor-pointer">Home</li>
+            <li className="cursor-pointer">About Us</li>
+            <li className="cursor-pointer">Testimonials</li>
+            <li className="cursor-pointer">Contact</li>
+            </ul></div>
+    </div>
+    </>
+    )
+}
+
+export default Navbar
