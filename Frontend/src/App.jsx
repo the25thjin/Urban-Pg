516b38ef2efd227bfc8ddacd13c9b24509b3f551
the25thjin <mannr075@gmail.com>
2024-08-15 21:02:05 +0530

    deployed on vercel

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,11 +2,13 @@ import React from "react"
 import Navbar from "./components/navbar"
 import Hero from "./components/hero"
 import Footer from "./components/footer"
+import About from "./components/about"
 function App() {
   return (
     <>
       <Navbar/>
       <Hero/>
+      <About/>
       <Footer/>
     </>
   )
--- Frontend/src/components/hero.jsx
+++ Frontend/src/components/hero.jsx
@@ -6,12 +6,12 @@ function Hero(){
         <div>
             <div className="h-screen bg-cover bg-no-repeat bg-center grid place-items-center" style={{backgroundImage: `url(${hero})`}}>
                 <div className="text-[#b9b8b8] font-Poppins text-center ">
-                <h1 className="font-bold text-[45px] ">Can't find your perfect place?</h1>
-                <h1 className="font-bold text-[45px] ">We've got you covered!</h1>
+                <h1 className="font-bold text-[43px] ">Can't find your perfect place?</h1>
+                <h1 className="font-bold text-[43px] ">We've got you covered!</h1>
 
-                <h2 className="font-normal mt-20 text-xl">Need affordable hostels, PGs, or flats near your college?</h2><br/>
-                <h2 className="font-normal text-xl">We’ve got you covered!  </h2><br/>
-                <h2 className="font-normal text-xl">Discover hidden gems for your perfect college home.</h2><br/>
+                <h2 className="font-normal mt-20 text-lg">Need affordable hostels, PGs, or flats near your college?</h2><br/>
+                <h2 className="font-normal text-lg">We’ve got you covered!  </h2><br/>
+                <h2 className="font-normal text-lg">Discover hidden gems for your perfect college home.</h2><br/>
 
                 <button className="bg-orange-700 text-white  h-10 w-40 rounded-xl mt-10 hover:bg-orange-800 ease-in-out">Your Next Home</button>
 
