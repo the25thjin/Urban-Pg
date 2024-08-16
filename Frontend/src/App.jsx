0a74fda8b7c8524eabaad9c380f0786748336b9b
the25thjin <mannr075@gmail.com>
2024-08-16 22:22:15 +0530

    why us done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,12 +3,15 @@ import Navbar from "./components/navbar"
 import Hero from "./components/hero"
 import Footer from "./components/footer"
 import About from "./components/about"
+import WhyUs from "./components/whyus"
+
 function App() {
   return (
     <>
       <Navbar/>
       <Hero/>
       <About/>
+      <WhyUs/>
       <Footer/>
     </>
   )
---
+++ Frontend/src/components/whyus.jsx
@@ -0,0 +1,19 @@
+function WhyUs(){
+    return(
+        <>
+        <div className="text-white text-xl z-[-1] relative left-[18%] top-[-300px]">
+           <span className="text-[#d4d4d4] font-Poppins font-extrabold text-[50px]"> Why Us? </span>
+          
+            <ul className=" mt-4 ml-10 list-disc list-inside" >  
+                <li >No Brokerage Fees</li>
+                <li>Affordable Options</li>
+                <li>User-Friendly Search</li>
+                <li>Genuine Reviews</li>
+
+            </ul>
+            </div>
+        
+        </>
+    )
+}
+export default WhyUs        
