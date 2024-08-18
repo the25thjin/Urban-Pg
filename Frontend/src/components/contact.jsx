7823434d990e8936d57298818258df5252dad072
the25thjin <mannr075@gmail.com>
2024-08-18 09:22:53 +0530

    minor updates in contact form

--- Frontend/src/components/contact.jsx
+++ Frontend/src/components/contact.jsx
@@ -7,21 +7,21 @@ function Contact(){
             <h1 className="text-[#d4d4d4] font-Poppins text-center  font-extrabold text-[50px]">Contact Us</h1>
         </div>
         <div className="flex justify-around items-center mt-20 ">
-            <div className="h-[420px] w-[400px]  flex-col  p-6 rounded-3xl">
+            <div className="h-[420px] w-[400px] bg-[#793406] flex-col  p-6 rounded-3xl">
                 <div>
                     <h1 className="">What's your name?</h1>
-                    <input className="text-black mt-2 w-[350px] h-[40px] rounded p-1 outline-none"  placeholder="Name"/>
+                    <input className=" bg-[#0e0901bd] mt-2 w-[350px] h-[40px] rounded p-1 outline-none"  placeholder="Name"/>
                 </div>
                 <div className="mt-7">
                     <h1>What's your email?</h1>
-                    <input className="text-black mt-2 w-[350px] h-[40px] rounded p-1 outline-none"  placeholder="Email"/>
+                    <input className="  bg-[#0e0901bd] mt-2 w-[350px] h-[40px] rounded p-1 outline-none"  placeholder="Email"/>
                 </div>
                 <div className="mt-7 mb-4">
                     <h1>Your Message</h1>
-                    <textarea className="text-black mt-2 w-[350px] h-[90px] rounded p-1 outline-none"  placeholder="Message"/>
+                    <textarea className="  bg-[#0e0901bd] mt-2 w-[350px] h-[90px] rounded p-1 outline-none"  placeholder="Message"/>
                 </div>
                 <div>
-                    <button className="w-[350px] bg-orange-800 h-[35px] rounded hover:bg-orange-900 ">Submit</button>
+                    <button className="w-[350px] bg-yellow-700 h-[35px] rounded hover:bg-yellow-800 ">Submit</button>
                 </div>
             </div>
             <div>
