55d80216fbcdef5cda593bb0e9ca7b12513ec25d
the25thjin <mannr075@gmail.com>
2024-08-21 01:47:11 +0530

    responsive for hero section

--- Frontend/src/components/hero.jsx
+++ Frontend/src/components/hero.jsx
@@ -1,4 +1,6 @@
 import hero from "../assets/hero.jpg"
+import hero2 from "../assets/hero2.jpg"
+import hero3 from "../assets/hero3.png"
 
 function Hero(){
     return(
@@ -6,14 +8,16 @@ function Hero(){
         <div>
             <div className="h-screen bg-cover bg-no-repeat bg-center grid place-items-center" style={{backgroundImage: `url(${hero})`}}>
                 <div className="text-[#b9b8b8] font-Poppins text-center ">
-                <h1 className="font-bold text-[43px] ">Can't find your perfect place?</h1>
-                <h1 className="font-bold text-[43px] ">We've got you covered!</h1>
+                <h1 className="font-bold text-[40px] sm:text-[43px] ">Can't find your perfect place?</h1>
+                <h1 className="font-bold text-[40px] sm:text-[43px] ">We've got you covered!</h1>
 
-                <h2 className="font-normal mt-20 text-lg">Need affordable hostels, PGs, or flats near your college?</h2><br/>
-                <h2 className="font-normal text-lg">We’ve got you covered!  </h2><br/>
-                <h2 className="font-normal text-lg">Discover hidden gems for your perfect college home.</h2><br/>
+                <img className="sm:hidden block " src={hero3} />
 
-                <button className="bg-orange-700 text-white  h-10 w-40 rounded-xl mt-10 transition duration-300 ease-in-out shadow-2xl shadow-orange-700 hover:bg-orange-900 hover:scale-110">Your Next Home</button>
+                <h2 className="sm:block hidden font-normal mt-20 text-lg">Need affordable hostels, PGs, or flats near your college?</h2><br/>
+                <h2 className="sm:block hidden font-normal text-lg">We’ve got you covered!  </h2><br/>
+                <h2 className="sm:block hidden font-normal text-lg">Discover hidden gems for your perfect college home.</h2><br/>
+
+                <button className="bg-orange-700 text-white  h-10 w-40 rounded-xl sm:mt-10 transition duration-300 ease-in-out shadow-2xl shadow-orange-700 hover:bg-orange-900 hover:scale-110">Your Next Home</button>
 
                 </div>
             </div>
