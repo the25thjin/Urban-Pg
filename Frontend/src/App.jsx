aa92325a204fe603bcb4587d6c11737e871b5f44
the25thjin <mannr075@gmail.com>
2024-09-28 21:54:26 +0530

    minor change

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,5 @@
 import React from "react"
-// import Navbar from "./components/Homepage/navbar"
-// import Hero from "./components/Homepage/hero"
-// import Footer from "./components/Homepage/footer"
-// import About from "./components/Homepage/about"
-// import WhyUs from "./components/Homepage/whyus"
-// import Testimonials from "./components/Homepage/testomonials"
-// import Contact from "./components/Homepage/contact"
+
 import Layout from "./components/Layout/Layout"
 import Login from "./components/Login/Login"
 import Signup from "./components/Login/Signup"
@@ -15,8 +9,6 @@ function App() {
   return (
     <BrowserRouter>
     <div className="overflow-x-hidden">
-      
-     
       <Routes>
        <Route path="/" element={<Layout/>}/> 
       <Route path="login" element={<Login/>}/>
